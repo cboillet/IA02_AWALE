eaadd7bb5a2acda0c6570c2416d28422786abed0
misterj <duthon.jean@gmail.com>
2014-05-21 17:13:44 +0200

    Update Predicats.pl

--- Predicats.pl
+++ Predicats.pl
@@ -11,7 +11,7 @@ ielement([T|Q],N,X):-ielement(Q,M,Y),N is M+1,X=Y,!.
 ielement([T|Q],1,T).
 
 %Prédicat jouer un tour jouer(J,C,L) J:joueur, C:case, L:liste
-jouer(J,C,L) :- peut_jouer(J,C), ielement(C,V,L), C is C+1, distribuer(V,C,L).
+jouer(J,C,L) :- peut_jouer(J,C), nbgraines(C,V,L), C is C+1, distribuer(V,C,L).
 
 %Prédicat test si l'joueur peut jouer cette case peut_jouer(J,C) J joue la case C
 peut_jouer(J,C) :- C<7, J=joueur1.
@@ -20,4 +20,4 @@ peut_jouer(J,C) :- C>6, J=joueur2.
 %Prédicat distribuer distribuer(C,G,L) C:Case de départ, G:nb de grainse, L:liste 
 distribuer(0,_,_).
 distribuer(G,13,L) :- distribuer(G,0,L).
-distribuer(G,C,L) :- ielement(C,V,L),V is V+1, G is G-1, C is C+1, distribuer(G,C,L).
+distribuer(G,C,L) :- nbgraines(C,V,L),V is V+1, G is G-1, C is C+1, distribuer(G,C,L).
